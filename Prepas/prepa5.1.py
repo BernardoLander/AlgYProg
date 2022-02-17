ba4ce9d8534f5290b755db7c82575af6998c8f10
BernardoLander <bernardoh.landerl@gmail.com>
2022-02-17 15:56:34 -0400

    prepa 17/2

---
+++ Prepas/prepa5.1.py
@@ -0,0 +1,5 @@
+from prepa5 import str_valid
+
+whoop = False
+
+print (str_valid(whoop))
---
+++ Prepas/prepa5.py
@@ -0,0 +1,18 @@
+#metodo para append a un diccionario
+
+def main():
+    s = {}
+    dni = 13
+    name = "abc"
+    s[dni] = name
+    print (s)
+
+def str_valid(msg):
+    while True:
+        if msg:
+
+            return 1
+
+        else:
+
+            return 2
